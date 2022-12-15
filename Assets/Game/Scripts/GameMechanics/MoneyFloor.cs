d49c54875a5a3394314d6b88d3cb636ccfc87a14
Furkan Tokkan <efurk64@gmail.com>
2022-12-15 11:56:13 +0300

    Target Change

--- Assets/Game/Scripts/GameMechanics/MoneyFloor.cs
+++ Assets/Game/Scripts/GameMechanics/MoneyFloor.cs
@@ -6,6 +6,7 @@ public class MoneyFloor : MonoBehaviour
 {
     public List<Transform> coins = new List<Transform>();
     private List<Transform> activeCoins = new List<Transform>();
+    [SerializeField] TargetData[] targetsToShoot;
 
     [SerializeField] Transform platformToMove;
 
@@ -16,6 +17,7 @@ public class MoneyFloor : MonoBehaviour
         activeCoins.AddRange(coins);
         Coin.onCoinInvisible += RemoveFromList;
         Coin.onCoinVisible += AddToList;
+        ActivateTargetToShoot(0, 0);
     }
     private void OnDisable()
     {
@@ -33,9 +35,36 @@ public class MoneyFloor : MonoBehaviour
         activeCoins.Add(transform);
     }
 
+    public void ActivateTargetToShoot(int buildingIndex, int incomeIndex)
+    {
+        coins.Clear();
+        activeCoins.Clear();
+
+        List<Transform> allChilds = new List<Transform>();
+        allChilds.AddRange(GetComponentsInChildren<Transform>());
+        allChilds.Remove(transform);
+        for (int i = 0; i < allChilds.Count; i++)
+        {
+            allChilds[i].gameObject.SetActive(false);
+            if (i == allChilds.Count - 1)
+            {
+                targetsToShoot[buildingIndex].targets[incomeIndex].gameObject.SetActive(true);
+                coins.AddRange(targetsToShoot[buildingIndex].targets[incomeIndex].GetComponentsInChildren<Transform>());
+                coins.Remove(targetsToShoot[buildingIndex].targets[incomeIndex].transform);
+                activeCoins.AddRange(coins);
+            }
+        }
+    }
+
+
     public Transform GetRandomActiveCoin()
     {
         int index = Random.Range(0, activeCoins.Count);
         return activeCoins[index];
     }
+}
+[System.Serializable]
+public class TargetData
+{
+    public GameObject[] targets;
 }
