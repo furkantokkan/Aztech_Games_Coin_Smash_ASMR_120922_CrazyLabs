fcf2529f57779910ecd7b8fcb6e545b58ef8e0dc
Furkan Tokkan <efurk64@gmail.com>
2023-01-21 05:18:52 +0300

    Giga Chad Canvas

--- Assets/Game/Scripts/GameMechanics/IncrementalIdle/UpgradeButton.cs
+++ Assets/Game/Scripts/GameMechanics/IncrementalIdle/UpgradeButton.cs
@@ -15,6 +15,7 @@ public class UpgradeButton : MonoBehaviour
     [SerializeField] private float inactiveAlpha;
     [Header("UI")]
     [SerializeField] private TextMeshProUGUI price;
+    [SerializeField] private GameObject spriteMaskObject;
 
     private IInteractable interaction;
 
@@ -53,7 +54,15 @@ public class UpgradeButton : MonoBehaviour
     {
         bool interactable = EconomyManager.Instance.GetCurrentMoney() >= item.CurrentPrice && item.currentLevel < item.maxLevel && interaction.CanInteract();
         group.interactable = interactable;
-        group.alpha = interactable ? 1f : inactiveAlpha;
+        if (interactable)
+        {
+            spriteMaskObject.SetActive(false);
+        }
+        else
+        {
+            spriteMaskObject.SetActive(true);
+        }
+        //group.alpha = interactable ? 1f : inactiveAlpha;
         price.text = item.currentLevel < item.maxLevel ? ((int)item.CurrentPrice).ToString() : "Max";
     }
 
